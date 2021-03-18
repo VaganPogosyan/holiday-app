92476a1534a1af5c7f943214613aa5f9da1f0b58
Vagan Pogosyan <vaganpogosyan@macbook-pro.myfiosgateway.com>
2021-03-18 00:10:36 -0400

    set up mongodb for deployment on heroku

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
+const db = mongoose.connection;
 
 const holidaysController = require('./controllers/holidays');
 
 const APP = express();
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 const DBNAME = 'holidays';
 
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/${DBNAME}`;
 
-mongoose.connect(`mongodb://localhost:27017/${DBNAME}`, { useNewUrlParser: true});
-mongoose.connection.once('open', () => {
-    console.log('connected to mongoose...');
-});
+// connect ot mongo
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
+
+// mongoose.connection.once('open', () => {
+//     console.log('connected to mongoose...');
+// });
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// open the connection to mongo
+db.on('open', () => { });
+
+/*
 const whitelist = ['http://localhost:3000']
 const corsOptions = {
     origin: function (origin, callback) {
@@ -24,8 +39,9 @@ const corsOptions = {
         }
     }
 }
+*/
 
-APP.use(cors(corsOptions));
+// APP.use(cors(corsOptions));
 
 APP.use(express.json());
 
